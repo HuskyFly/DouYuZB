a21c827fc23d33903587171b7210e6c249048db1
Mr.M <xx97923145@126.com>
2020-02-27 10:25:22 +0800

    初始化项目

---
+++ DouYuZB/DouYuZB/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DouYuZB
+//
+//  Created by Mr.M on 2020/2/27.
+//  Copyright © 2020 Mr.M. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ DouYuZB/DouYuZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DouYuZB
+//
+//  Created by Mr.M on 2020/2/27.
+//  Copyright © 2020 Mr.M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
